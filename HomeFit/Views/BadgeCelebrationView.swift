51899ec3d738095175e16cc19e3d859f8a29cd80
mhdtrk10 <99149415+mhdtrk10@users.noreply.github.com>
2025-08-02 15:11:50 +0300

    güncelleme
    Bu repository'de konfeti ile görsel güzellik ekledim. 10. 15. 20. gün gibi serileri tamamladığında ekrana konfeti ile kutlama geliyor.

--- HomeFit/Views/BadgeCelebrationView.swift
+++ HomeFit/Views/BadgeCelebrationView.swift
@@ -6,17 +6,19 @@
 //
 
 import SwiftUI
+import ConfettiSwiftUI
 
 struct BadgeCelebrationView: View {
     
     let badgeEmoji: String
     let badgeTitle: String
+    @State private var confettiTrigger = 0
     @Environment(\.dismiss) var dismiss
     
     
     var body: some View {
         VStack(spacing: 20) {
-            Spacer()
+            
             
             Text("Tebrikler!")
                 .font(.largeTitle)
@@ -41,7 +43,18 @@ struct BadgeCelebrationView: View {
             .background(Color.blue)
             .foregroundColor(Color.white)
             .cornerRadius(12)
-            
+            .confettiCannon(
+                trigger: $confettiTrigger,
+                num: 50,
+                colors: [.pink, .green, .orange],
+                rainHeight: 600,
+                openingAngle: Angle(degrees: 60),
+                closingAngle: Angle(degrees: 120),
+                radius: 300
+            )
+            .onAppear {
+                confettiTrigger += 1
+            }
         }
         .padding()
         .background(Color.white)
